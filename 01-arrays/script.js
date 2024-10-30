14e1ec1795bc0cf80ae123bc3619eebfa750bd03
EnricoGenovese <enricogeno@libero.it>
2024-10-30 15:58:11 +0100

    01 - arrays: exercise 1 split solution in two lines (show actual sostitution)

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -11,7 +11,8 @@ const teachers = [
 // 1. Utilizzando un indice sull'array teachers, cambia il valore della
 // variabile fourthTeacher qui sotto in modo che sia il quarto insegnante elencato nell'array
 
-const fourthTeacher = console.log(teachers[3]);
+const fourthTeacher = teachers[3];
+console.log(teachers[3]);
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
 
