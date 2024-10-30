98747bb323834bf5f7e4d4c2ebb5bffde1494cbb
EnricoGenovese <enricogeno@libero.it>
2024-10-30 15:54:19 +0100

    02 - arrays-bonus: comment filter function for future reference; exercise 3 (solution for reversed array also);

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -18,7 +18,27 @@ console.log(reversedTeachers);
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
 
 const longNames = teachers.filter((word) => word.length >= 5);
+
+// la funzione filter() possiede una callbackFn(call back function), che itera per ogni elemento dell'array
+// e restituisce un boolean (un ciclo for non esplicitato); crea una copia (o clone) dell'array originale
+// contenente tutti gli elementi che soddifano la condizione indicata dopo l'indicatore "=>" della funzione.
+
+// Questa funzione viene chiamata con i seguenti argomenti:
+//  - l'elemento dell'array processato al momento; (in questo caso una stringa)
+//  - l'index dell'elemento processato al momento; 
+//  - l'array sul quale la funzione 'filter()' ha effetto.
+
+
 console.log(longNames);
 
 // 3. Rimuovi 'Ed' dall'array teachers
 
+// se si utilizza l'array originale:
+
+// teachers.splice(1, 1);
+// console.log(teachers);
+
+// se si utilizza l'array invertito (es. 1); funziona solamente in sostituzione del medoto sopra:
+
+teachers.splice(5, 1);
+console.log(teachers);
